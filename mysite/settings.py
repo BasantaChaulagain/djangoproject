97a6ff2946162de102f11bb2406221ecf3e92bb2
Basanta Chaulagain <basanta83chaulagain@gmail.com>
2016-12-19 23:48:16 +0545

    changed staticfiles to static

--- mysite/settings.py
+++ mysite/settings.py
@@ -116,11 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
